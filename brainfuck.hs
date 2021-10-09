2c69b64853008af480bde23082326ea7c6653dc2
Ivan Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2021-10-09 13:06:04 -0500

    test

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,70 +1,122 @@
 
 
 
-type Tape = ([Int], Int, [Int])
-type Instructions = ([Char], Char, [Char])
+import Data.Maybe (isJust)
+import Data.List (elemIndex)
+type Tape = ([Int], Int)
+-- type Instructions = Token
 
-mRIP :: Instructions -> Instructions
-mRIP ([], f, h:hs) = ([f], h, hs)
-mRIP (fs, f, []) = (fs ++ [f], ' ', [])
-mRIP (fs, f, h:hs) = (fs ++ [f], h, hs)
+data Token = 
+    TLeft    {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TRight   {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TPlus    {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TMinus   {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TPrint   {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TRead    {left :: (Maybe Token), right :: (Maybe Token), val :: Int } |
+    TFirst   {left :: (Maybe Token), right :: (Maybe Token)             } |
+    TLast    {left :: (Maybe Token), right :: (Maybe Token)             } |
+    TEnd -- Funny, isn't it?
+    deriving (Eq, Show)
 
-moveRight :: Instructions -> Tape -> String
-moveRight xs ([], x, []) = machine xs ([x], 0, [])
-moveRight xs ([], x, y:ys) = machine xs ([x], y, ys)
-moveRight xs (ys, x, []) = machine xs (ys ++ [x], 0, [])
-moveRight xs (ys, x, t:ts) = machine xs (ys ++ [x], t, ts)
 
-moveLeft :: Instructions -> Tape -> String
-moveLeft _ ([], _, _) = error "Parse error!"
-moveLeft xs (ys, x, []) = machine xs (init ys, last ys, [x])
-moveLeft xs (ys, x, ts) = machine xs (init ys, last ys, x : ts)
+moveRight :: Token -> Token -> Tape -> Int -> String
+moveRight x y 0 = runner x y
+moveRight x (ys, pos) t
+    | isJust (elemIndex newIndex ys) = runner x (ys, newIndex)
+    | otherwise = runner x (ys ++ replicate (newIndex - lastIndex) 0, newIndex)
+        where   newIndex = pos + t
+                lastIndex = length ys - 1
 
-plus :: Instructions -> Tape -> String
-plus xs (ys, x, ts) = machine xs (ys, x + 1, ts)
+moveLeft :: Token -> Token -> Tape -> Int -> String
+moveLeft x y 0 = runner x y
+moveLeft x (ys, pos) t
+    | isJust (elemIndex newIndex ys) = runner x (ys, newIndex)
+    | otherwise = error "index < -1"
+        where   newIndex = pos - t
 
-minus :: Instructions -> Tape -> String
-minus xs (ys, x, ts) = machine xs (ys, x - 1, ts)
 
-loopInit :: Instructions -> Tape -> String
-loopInit xs (ys, 0, ts) = machine (jumpToEndLoop xs) (ys, 0, ts)
-loopInit xs t = machine xs t
+plus :: Token -> Token -> Tape -> Int -> String
+plus x y 0 = runner x y
+plus x (y:ys, pos) t = runner x ((y + t):ys, pos)
+plus _ ([], _) _ = error "empty list"
 
-loopEnd :: Instructions -> Tape -> String
-loopEnd xs (ys, 0, ts) = machine xs (ys, 0, ts)
-loopEnd xs t = machine (jumpToStartLoop xs) t
+minus :: Token -> Token -> Tape -> Int -> String
+minus x y 0 = runner x y
+minus x (y:ys, pos) t = runner x ((y - t):ys, pos)
+minus _ ([], _) _ = error "empty list"
+
+loopInit :: Token -> Token -> Tape -> String
+loopInit x (0 : ys, pos) = runner (jumpToEndLoop x) (0 : ys, pos)
+loopInit x y = runner t y
+    where t = 
+
+-- loopEnd :: Instructions -> Tape -> String
+
 
 -- Something isn't working in this two functions
-jumpToEndLoop :: Instructions -> Instructions
-jumpToEndLoop (fs, '[', h:hs) = jumpToEndLoop (jumpToEndLoop (fs ++ ['['], h, hs))
-jumpToEndLoop (fs, ']', h:hs) = (fs ++ [']'], h, hs)
-jumpToEndLoop (fs, ']', []) = (fs ++ [']'], ' ', [])
-jumpToEndLoop (_, _, []) = error "bad syntax"
-jumpToEndLoop (fs, x, h:hs) = jumpToEndLoop (fs ++ [x], h, hs)
 
-jumpToStartLoop :: Instructions -> Instructions
-jumpToStartLoop (fs, ']', hs) = let (a,b,c) = (jumpToStartLoop (init fs, last fs, ']':hs))
-    in jumpToStartLoop (init a, last a, b:c)
-jumpToStartLoop (fs, '[', hs) = (fs, '[', hs)
-jumpToStartLoop ([], _, _) = error "bad syntax"
-jumpToStartLoop (fs, x, hs) = jumpToStartLoop (init fs, last fs, x:hs)
+-- [ --------> ]
+jumpToEndLoop :: Token -> Token
+jumpToEndLoop (TLast _ (Just r)) = r
+jumpToEndLoop (TFirst _ (Just r)) = jumpToEndLoop r
+jumpToEndLoop (TLast _ Nothing) = error "Unexpected"
+jumpToEndLoop (TFirst _ Nothing) = error "Unexpected"
+jumpToEndLoop TEnd = error "END"
+jumpToEndLoop e = case right e of
+    Nothing -> error "END"
+    Just a -> jumpToEndLoop a
 
-printC :: Instructions -> Tape -> String
-printC f (xs, x, ys) = (toEnum x :: Char ) : machine f (xs, x, ys)
 
-machine :: Instructions -> Tape -> String
-machine (ys, '>', x:xs) t = moveRight (ys ++ ['>'], x, xs) t
-machine (ys, '<', x:xs) t = moveLeft (ys ++ ['<'], x, xs) t
-machine (ys, '+', x:xs) t = plus (ys ++ ['+'], x, xs) t
-machine (ys, '-', x:xs) t = minus (ys ++ ['-'], x, xs) t
-machine (ys, '.', x:xs) t = printC (ys ++ ['.'], x, xs) t
--- machine (ys, ',', x:xs) t = moveRight (ys ++ [','], x, xs) t
-machine (ys, '[', x:xs) t = loopInit (ys ++ ['['], x, xs) t
-machine (ys, ']', x:xs) t = loopEnd (ys ++ [']'], x, xs) t
-machine (_, _, []) _ = ""
-machine _ _ = error ""
+-- [ <-------- ]
+jumpToStartLoop :: Token -> Token
+jumpToEndLoop (TLast Just l r) = TLast Nothing r
 
-run :: String -> String
-run (t:ts) = machine ([], t, ts) ([], 0, [])
-run [] = ""
+-- printC :: Instructions -> Tape -> String
 
+printC :: Token -> Tape -> Int -> String
+printC f (xs, pos) c =  outchars ++ runner f (xs, pos)
+    where outchars = replicate c (toEnum (xs !! pos) :: Char )
+
+isChar :: Char -> (Char -> Bool)
+isChar x = f where
+    f y = x == y
+
+howChars :: String -> (Char -> Bool) -> (Int, String)
+howChars "" _ = (0, "")
+howChars (x:xs) f
+    | f x = let (a, b) = howChars xs f in (a + 1, b)
+    | otherwise = (0, x:xs)
+
+tokenizer :: String -> Token
+tokenizer ('>':xs) = let (tok, str) = howChars ('>':xs) (isChar '>')
+    in TRight Nothing (Just (tokenizer str)) tok
+tokenizer ('<':xs) = let (tok, str) = howChars ('<':xs) (isChar '<')
+    in TLeft Nothing (Just (tokenizer str)) tok
+tokenizer ('+':xs) = let (tok, str) = howChars ('+':xs) (isChar '+')
+    in TPlus Nothing (Just (tokenizer str)) tok
+tokenizer ('-':xs) = let (tok, str) = howChars ('-':xs) (isChar '-')
+    in TMinus Nothing (Just (tokenizer str)) tok
+tokenizer ('.':xs) = let (tok, str) = howChars ('.':xs) (isChar '.')
+    in TPrint Nothing (Just (tokenizer str)) tok
+tokenizer (',':xs) = let (tok, str) = howChars (',':xs) (isChar ',')
+    in TRead Nothing (Just (tokenizer str)) tok
+tokenizer ('[':xs) = TFirst Nothing (Just (tokenizer xs))
+tokenizer (']':xs) = TLast Nothing (Just (tokenizer xs))
+tokenizer "" = TEnd
+tokenizer (_:xs) = tokenizer xs
+-- tokenizer _ = error "Oh, fiddlesticks! What now?"
+
+runner :: Token -> Tape -> String
+runner (TRight _ x v) y = error "Oh, fiddlesticks! What now?"
+runner _ _ = error "Oh, fiddlesticks! What now?"
+
+machine :: String -> String
+machine "" = error "Oh, fiddlesticks! What now?"
+machine x = runner (tokenizer x) ([0], 0)
+
+-- run :: String -> String
+-- run (t:ts) = machine ([], t, ts) ([], 0, [])
+-- run [] = ""
+
+main :: IO ()
+main = putStrLn (machine "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
