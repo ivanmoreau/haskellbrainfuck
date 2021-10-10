32a26beaff68db3bf15d3e72b3a3196cc75db0b2
Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2021-10-10 02:38:21 +0000

    Delete comments

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-
 import Data.Maybe (isJust, fromJust)
 import Data.List (elemIndex)
 import Control.Exception.Base (tryJust)
@@ -41,70 +40,37 @@ moveLeft x (ys, pos) t
     | otherwise = error "index < -1"
         where   newIndex = pos - t
 
-
 plus :: Instructions -> Tape -> Int -> String
 plus x y 0 = runner x y
---plus x (y:ys, pos) t = runner x ((y + t):ys, pos)
 plus x (ys, pos) t
     | isJust result = runner x (take pos ys ++ (fromJust result + t) : drop (pos + 1) ys, pos)
     | otherwise = error "Evil index."
     where result = ys >! pos
---plus _ ([], _) _ = error "Unreachable case: empty list."
 
 minus :: Instructions -> Tape -> Int -> String
 minus x y 0 = runner x y
---minus x (y:ys, pos) t = runner x ((y - t):ys, pos)
 minus x (ys, pos) t
     | isJust result = runner x (take pos ys ++ (fromJust result - t) : drop (pos + 1) ys, pos)
     | otherwise = error "Evil index."
     where result = ys >! pos
---minus _ ([], _) _ = error "Unreachable case: empty list."
 
 loopInit :: Instructions -> Tape -> Int -> String
--- loopInit (xs, _) (0 : ys, pos) t = runner (xs, t) (0 : ys, pos)
 loopInit (xs, x) (ys, pos) t
     | isJust result = case fromJust result of
         0 -> runner (xs, t) (ys, pos)
         _ -> runner (xs, x) (ys, pos)
     | otherwise = error "Evil index."
     where result = ys >! pos
--- loopInit x y _ = runner x y
 
 loopEnd :: Instructions -> Tape -> Int -> String
--- loopEnd x (0 : ys, pos) _ = runner x (0 : ys, pos)
 loopEnd (xs, x) (ys, pos) t
     | isJust result = case fromJust result of
         0 -> runner (xs, x) (ys, pos)
         _ -> runner (xs, t) (ys, pos)
     | otherwise = error "Evil index."
     where result = ys >! pos
--- loopEnd (xs, _) y t = runner (xs, t) y
-
--- Something isn't working in this two functions
-
--- [ --------> ]
--- jumpToEndLoop :: Instructions -> Instructions
--- jumpToEndLoop (TLast t:xs, pos) = (TLast t:xs, pos + 1)
--- jumpToEndLoop (TFirst t:xs, pos) = jumpToEndLoop (jumpToEndLoop (TFirst t:xs, pos + 1))
--- jumpToEndLoop (x:xs, pos) = jumpToEndLoop (x:xs, pos + 1)
--- jumpToEndLoop ([], _) = error "EOF: \"]\" was expected."
-
-
--- [ <-------- ]
--- jumpToStartLoop :: Instructions -> Instructions
--- jumpToStartLoop (TFirst t:xs, pos) = (TFirst t:xs, pos + 1)
--- jumpToStartLoop (TLast t:xs, pos) = let (a, b) = jumpToStartLoop (TLast t:xs, pos + 1)
---     in jumpToStartLoop (a, b - 2)
--- jumpToStartLoop (x:xs, pos) = jumpToEndLoop (x:xs, pos - 1)
--- jumpToStartLoop ([], _) = error "SOF: \"[\" was expected."
-
--- printC :: Instructions -> Tape -> String
 
 printC :: Instructions -> Tape -> Int -> String
---printC f ([0], 0) c =  outchars ++ runner f ([0], 0)
---    where outchars = replicate c (toEnum ([0] !! 0) :: Char )
---printC f (xs, pos) c = error (show (xs, pos))
---printC (i, ip) (xs, pos) c = concat (replicate c (" ip: " ++ show (i !! ip) ++ ", " ++ show pos ++ ":" ++ show (xs !! pos) ++ " ")) ++ runner (i, ip) (xs, pos)
 printC f (xs, pos) c =  outchars ++ runner f (xs, pos)
     where outchars = replicate c (toEnum (xs !! pos) :: Char )
 
@@ -118,8 +84,6 @@ howChars (x:xs) f
     | f x = let (a, b) = howChars xs f in (a + 1, b)
     | otherwise = (0, x:xs)
 
-
--- WORKING, DON'T TOUCH. 🤷🏼‍♀️
 tokenizer :: String -> Int -> [Int] -> ([Token], [Int])
 tokenizer ('>':xs) t q = let (tok, str) = howChars ('>':xs) (isChar '>')
     in let (tokens, ind) = tokenizer str (t + 1) q in (TRight tok : tokens, ind)
@@ -146,9 +110,7 @@ tokenizer (']':xs) t q =
     in (TLast i : tokens, t:ind)
 tokenizer "" _ _ = ([], [])
 tokenizer (_:xs) t q = tokenizer xs t q
--- tokenizer _ = error "Oh, fiddlesticks! What now?"
 
--- Probably working. 🤷🏼‍♀️
 runner :: Instructions -> Tape -> String
 runner (xs, pos) y = if (pos + 1) > length xs
     then "" else (case (xs !! pos) of
@@ -160,21 +122,10 @@ runner (xs, pos) y = if (pos + 1) > length xs
         TRead _ -> error "unimplemented"
         TFirst t -> loopInit (xs, pos + 1) y t
         TLast t -> loopEnd (xs, pos + 1) y t)
--- runner _ _ = error "Oh, fiddlesticks! What now?"
 
--- WORKING, DON'T TOUCH. 🤷🏼‍♀️
 machine :: String -> String
 machine "" = error "Oh, fiddlesticks! What now?"
 machine x = runner (fst (tokenizer x 0 []), 0) ([0], 0)
 
--- run :: String -> String
--- run (t:ts) = machine ([], t, ts) ([], 0, [])
--- run [] = ""
-
---debugTokens :: [Token] -> Int -> String
---debugTokens (x:xs) t = (show t) ++ ": " ++ (show x) ++ "     " ++ debugTokens xs (t + 1)
---debugTokens [] _ = "    "
-
--- WORKING, DON'T TOUCH. 🤷🏼‍♀️
 main :: IO ()
 main = putStrLn (machine "++++++++.[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
